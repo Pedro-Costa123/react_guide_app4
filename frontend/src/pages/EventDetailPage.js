2fdba0785911942128b1d2803e44a3d228713734
Pedro-Costa123 <pedrocostaalves@live.com.pt>
2023-02-06 17:27:27 +0000

    Section 20 : 288

---
+++ frontend/src/pages/EventDetailPage.js
@@ -0,0 +1,14 @@
+import { useParams } from "react-router-dom";
+
+const EventsDetailPage = () => {
+  const params = useParams();
+
+  return (
+    <>
+      <h1>Events Detail Page</h1>
+      <p>Event ID: {params.eventId}</p>
+    </>
+  );
+};
+
+export default EventsDetailPage;
