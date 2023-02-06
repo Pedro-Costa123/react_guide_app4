d5268177c01a0a4b555df10214097c2f2c077e04
Pedro-Costa123 <pedrocostaalves@live.com.pt>
2023-02-06 12:43:33 +0000

    Section 20 : 280

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,14 +4,14 @@ const HomePage = () => {
   const navigate = useNavigate();
 
   const navigationHandler = () => {
-    navigate('/products')
+    navigate("products");
   };
 
   return (
     <>
       <h1>My Home Page</h1>
       <p>
-        Go To <Link to="/products">the list of products</Link>
+        Go To <Link to="products">the list of products</Link>
       </p>
       <p>
         <button onClick={navigationHandler}></button>
