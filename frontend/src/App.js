8db632ec3fe17e46bccb72a826856a304a10fb2f
Pedro-Costa123 <pedrocostaalves@live.com.pt>
2023-02-07 16:59:16 +0000

    Section 20 : 302

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,14 @@ import HomePage from "./pages/HomePage";
 import EventsPage, { loader as eventsLoader } from "./pages/EventsPage.js";
 import EventDetailPage, {
   loader as eventDetailLoader,
+  action as deleteEventAction,
 } from "./pages/EventDetailPage";
-import NewEventPage, { action as newEventAction } from "./pages/NewEventPage";
+import NewEventPage from "./pages/NewEventPage";
 import EditEventPage from "./pages/EditEventPage";
 import RootLayout from "./pages/Root";
 import EventsRoot from "./pages/EventsRoot";
 import ErrorPage from "./pages/Error";
+import { action as formEventAction } from "./components/EventForm";
 
 const router = createBrowserRouter([
   {
@@ -35,11 +37,16 @@ const router = createBrowserRouter([
               {
                 index: true,
                 element: <EventDetailPage />,
+                action: deleteEventAction,
               },
-              { path: ":edit", element: <EditEventPage /> },
+              {
+                path: ":edit",
+                element: <EditEventPage />,
+                action: formEventAction,
+              },
             ],
           },
-          { path: "new", element: <NewEventPage />, action: newEventAction },
+          { path: "new", element: <NewEventPage />, action: formEventAction },
         ],
       },
     ],
